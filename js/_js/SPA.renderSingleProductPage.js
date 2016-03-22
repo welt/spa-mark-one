b73e190892098b4799757c971af8633915827c5f
welt <welt@mac.com>
2016-03-22 15:03:49 +0000

    fixes image data

--- js/_js/SPA.renderSingleProductPage.js
+++ js/_js/SPA.renderSingleProductPage.js
@@ -18,7 +18,7 @@ var SPA = SPA || {};
 				if ( item.id == index ){
 					// Populate '.preview-large' with the chosen product's data.
 					container.find('h3').text(item.name);
-					container.find('img').attr('src', item.image.large);
+					container.find('img').attr('src', item.images.large);
 					container.find('p').text(item.description);
 				}
 			});
